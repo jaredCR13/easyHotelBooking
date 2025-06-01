d72db002c2e95591a6bd0cd916faf2431c08111b
jaredCR13 <jaredmor78@gmail.com>
2025-05-31 19:26:11 -0600

    FrontDeskClerk CRUD casi listo

--- hotelBookingSystem/src/main/java/com/easyhotelbooking/hotelbookingsystem/controller/logincontroller/LogInController.java
+++ hotelBookingSystem/src/main/java/com/easyhotelbooking/hotelbookingsystem/controller/logincontroller/LogInController.java
@@ -1,4 +1,12 @@
 package com.easyhotelbooking.hotelbookingsystem.controller.logincontroller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LogInController {
+
+    @FXML
+    public void logInOnAction(ActionEvent event) {
+
+    }
 }
